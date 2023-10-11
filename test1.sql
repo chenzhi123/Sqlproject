5f2bb47071ec52512274915e882beebf8460faf3
chenzhi123 <1049569420@qq.com>
2023-10-11 17:42:32 -0400

    1

--- test1.sql
+++ test1.sql
@@ -1,17 +1,24 @@
+--连接到database
 -- !preview conn=DBI::dbConnect(RSQLite::SQLite())
 
+--进入sqlite模式
 -- $sqlite3 tycho1.db 
 
-
+--查看前10行
 --SELECT * FROM tycho LIMIT 10;
+
+-- 查看行
 --select count(*) from tycho;
 
 
+--查看列
 --pragma table_info(tycho);
 
-
+-- 查看创建表格的语句
 --.scheme
 
+
+-- 美观
 --.mode table;
 
 
