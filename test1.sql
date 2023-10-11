834a14e261b762be9ffa6636f9cdc00c68deaf40
chenzhi123 <1049569420@qq.com>
2023-10-11 17:37:34 -0400

    1

---
+++ test1.sql
@@ -0,0 +1,17 @@
+-- !preview conn=DBI::dbConnect(RSQLite::SQLite())
+
+-- $sqlite3 tycho1.db 
+
+
+--SELECT * FROM tycho LIMIT 10;
+--select count(*) from tycho;
+
+
+--pragma table_info(tycho);
+
+
+--.scheme
+
+--.mode table;
+
+
